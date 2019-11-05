0dd3f9a613e8b503bcd45c1c429ab3ea5e47d9cd
Plyaka1994 <Plyaka1994@gmail.com>
2019-11-05 20:52:13 +0200

    Add Apps

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
- 
+const app2 = express()
+
 app.get('/',(req, res) => {
   res.sendFile(`${__dirname}/public/index.html`)
 })
-app.get('/api',(req, res) => {
+app2.get('/api',(req, res) => {
       res.sendFile(`${__dirname}/public/api.html`)
 })
  
 app.listen(3000, '0.0.0.0', () => {
     console.warn('Server express in https://0.0.0.0:3000')
+})
+app2.listen(4000, '0.0.0.0', () => {
+    console.warn('Server express in https://0.0.0.0:4000')
 })
